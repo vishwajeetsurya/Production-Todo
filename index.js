86b43673aaa8a4b6d8769e4b3ca36148e31f3930
Vishwaa <vish@gmail.com>
2024-02-09 19:00:42 +0530

    ui added

--- index.js
+++ index.js
@@ -2,14 +2,18 @@ const express = require("express")
 require("dotenv").config({ path: "./.env" })
 const mongoose = require("mongoose")
 const cors = require("cors")
-
+const path = require("path")
 mongoose.connect(process.env.MONGO_URL)
 const app = express()
 app.use(express.json())
 app.use(cors())
 
-app.use("/api/admin", require("./routes/todoRoute"))
+app.use(express.static(path.join(__dirname, "dist")))
 
+app.use("/api/admin", require("./routes/todoRoute"))
+app.use("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "dist", "index.html"))
+})
 mongoose.connection.once("open", () => {
     console.log("MONGO CONNECTED");
     app.listen(5000, console.log("server Running"))
